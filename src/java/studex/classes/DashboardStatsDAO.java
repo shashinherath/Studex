0e16b7c2b14ac2a139318421ad7cba7d5d3d795b
shashinherath <shashin.malinda@gmail.com>
2024-12-07 12:41:58 +0530

    Fix Dashboard stats

--- src/java/studex/classes/DashboardStatsDAO.java
+++ src/java/studex/classes/DashboardStatsDAO.java
@@ -5,20 +5,32 @@ import studex.classes.DBHelper;
 
 public class DashboardStatsDAO {
 
-    public int getCount(String tableName) {
+    public int getCount(String tableName, String userType) {
         int count = 0;
-        String query = "SELECT COUNT(*) FROM " + tableName;
+        String query;
 
-        try (Connection conn = DBHelper.getConnection();
-             PreparedStatement statement = conn.prepareStatement(query);
-             ResultSet resultSet = statement.executeQuery()) {
+        if ("user".equals(tableName)) {
+            query = "SELECT COUNT(*) FROM " + tableName + " WHERE user_type = ?";
+        } else {
+            query = "SELECT COUNT(*) FROM " + tableName;
+        }
 
-            if (resultSet.next()) {
-                count = resultSet.getInt(1);
+        try (Connection conn = DBHelper.getConnection(); PreparedStatement statement = conn.prepareStatement(query)) {
+
+            if ("user".equals(tableName)) {
+                statement.setString(1, userType);
             }
+
+            try (ResultSet resultSet = statement.executeQuery()) {
+                if (resultSet.next()) {
+                    count = resultSet.getInt(1);
+                }
+            }
+
         } catch (Exception e) {
             e.printStackTrace();
         }
+
         return count;
     }
 }
