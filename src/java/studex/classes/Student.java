3c0fa677b8f1fd6cc6270c4ecc6542f252473a19
shashinherath <shashin.malinda@gmail.com>
2024-12-31 21:59:48 +0530

    Update Student.java

--- src/java/studex/classes/Student.java
+++ src/java/studex/classes/Student.java
@@ -8,38 +8,55 @@ public class Student {
     private String phoneNo;
     private String enrollmentDate;
     private String guardianName;
-
-    public Student(int userId, String name, String email, String phoneNo, String enrollmentDate) {
+    private String className;
+     
+    public Student(int userId, String name, String email, String phoneNo, String enrollmentDate, String className) {
         this.userId = userId;
         this.name = name;
         this.email = email;
         this.phoneNo = phoneNo;
+        this.className = className;
         this.enrollmentDate = enrollmentDate;
     }
 
-    public Student(int userId, String name, String email, String phoneNo, String enrollmentDate, String guardianName) {
-        this.userId = userId;
+    public Student(String name, String email, String phoneNo, String enrollmentDate, String className) {
         this.name = name;
         this.email = email;
         this.phoneNo = phoneNo;
         this.enrollmentDate = enrollmentDate;
-        this.guardianName = guardianName;
+        this.className = className;
     }
 
     public int getUserId() {
         return userId;
     }
 
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
     public String getName() {
         return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 
     public String getEmail() {
         return email;
     }
 
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
     public String getPhoneNo() {
         return phoneNo;
+    }
+
+    public void setPhoneNo(String phoneNo) {
+        this.phoneNo = phoneNo;
     }
 
     public String getEnrollDate() {
@@ -49,4 +66,17 @@ public class Student {
     public String getGuardianName() {
         return guardianName;
     }
+
+    public void setEnrollDate(String enrollmentDate) {
+        this.enrollmentDate = enrollmentDate;
+    }
+
+    public String getClassName() {
+        return className;
+    }
+
+    public void setClassName(String className) {
+        this.className = className;
+    }
+
 }
